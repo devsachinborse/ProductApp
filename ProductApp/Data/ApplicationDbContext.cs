d6edcd36df763cc15e30f6cc3771d33a36911c93
Sachin Borse <dev.sachinborse@gmail.com>
2025-07-07 15:13:21 +0530

    Add project files.

---
+++ ProductApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApp.Models;
+
+namespace ProductApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        //add a dummy data into table
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 3 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 2 }
+                );
+        }
+    }
+}
